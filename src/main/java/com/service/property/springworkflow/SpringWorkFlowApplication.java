85eac2eec9787bcc24ba19b546b460e919731686
kyeiiih <kevin.kyei@systechafrica.com>
2024-05-01 11:48:41 +0300

    Initial commit

---
+++ src/main/java/com/service/property/springworkflow/SpringWorkFlowApplication.java
@@ -0,0 +1,13 @@
+package com.service.property.springworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWorkFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWorkFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/service/property/springworkflow/SpringWorkFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.property.springworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWorkFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
